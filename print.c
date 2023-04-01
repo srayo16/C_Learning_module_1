91c22e38a9e0a71613a72c18a49725f666b7f218
srayo16 <srayosikder@gmail.com>
2023-04-02 01:49:30 +0600

    printing int

--- print.c
+++ print.c
@@ -2,7 +2,16 @@
 
 int main()
 {
-    printf("Hello World!");
+    printf("Hello World!\n");
+    printf("Hello World! 2\n");
+    printf("New tabbing\t\t Hello! \n");
+    printf("\\\n");
+    printf("%%");
+
+    // \n new line
+    // \t tab
+    // %% = %
+    // \\ = \
 
     return 0;
 }
---
+++ variable.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+    int phitron = 10;
+    printf("Value: %d", phitron);
+
+    return 0;
+}
